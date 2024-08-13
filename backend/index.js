93e7fd6d0e5bed560e470af6e474ffb9ac995a43
Anish Pawaskar <anishpawaskar@gmail.com>
2024-08-14 02:00:02 +0530

    fix import of database config file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectToDb } from "./src/config/database";
+import { connectToDb } from "./src/config/database.js";
 
 dotenv.config();
 
