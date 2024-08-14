b5950ba5d9a14263480bf80a9ab75e7aacb88c03
Anish Pawaskar <anishpawaskar@gmail.com>
2024-08-15 01:09:06 +0530

    add cros and configure it

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectToDb } from "./src/config/database.js";
 import indexRouter from "./src/routes/index.js";
 
@@ -8,6 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectToDb();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 app.use(indexRouter);
 
