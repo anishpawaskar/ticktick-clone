c6e8060ab0cfd4d540b89edce3a9ddad02a68eba
Anish Pawaskar <anishpawaskar@gmail.com>
2024-08-17 21:22:59 +0530

    change import of TaskControlPanelPresentation

--- frontend/src/components/TaskControlPanel/index.jsx
+++ frontend/src/components/TaskControlPanel/index.jsx
@@ -2,7 +2,7 @@ import {
   LIST_DATA,
   TAGS_DATA,
 } from "./components/TaskListCarousel/taskListCarousel.constants";
-import { TaskControlPanelPresentation } from "./presentation";
+import { TaskControlPanelPresentation } from "./Presentation";
 
 export const TaskControlPanel = () => {
   return <TaskControlPanelPresentation lists={LIST_DATA} tags={TAGS_DATA} />;
