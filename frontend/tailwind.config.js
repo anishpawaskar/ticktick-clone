2635ade2afb8177404af60642d7bca31cd8dd6d9
Anish Pawaskar <anishpawaskar@gmail.com>
2024-08-22 02:38:54 +0530

    refactor: add new viewport point for media query

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: "500px",
+      xs: "500px",
+      sm: "630px",
       md: "900px",
       lg: "950px",
       xl: "1440px",
