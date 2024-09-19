12c463e3d3df8da3e225eb778c90afd0851abaa9
Anish Pawaskar <anishpawaskar@gmail.com>
2024-09-20 02:56:41 +0530

    refactor: sidebar

--- frontend/src/components/Sidebar/Presentation.jsx
+++ frontend/src/components/Sidebar/Presentation.jsx
@@ -1,48 +1,49 @@
-import { ImUser } from "react-icons/im";
-import { IoSync } from "react-icons/io5";
-import { MdNotifications } from "react-icons/md";
-import { HiQuestionMarkCircle } from "react-icons/hi";
-import { iconsComponents, USER_SELECTED_FEATURES } from "./sidebarConstants";
-import { Link } from "react-router-dom";
+import { RiUser3Fill } from "react-icons/ri";
+import { FaCrown } from "react-icons/fa6";
+import { USER_FEATURES, APP_FEATURES } from "./sidebarConstants";
 
 export const SidebarPresentation = () => {
   return (
-    <div className="hidden xs:flex h-full w-[50px] flex-col items-center border-solid border-gray-50 bg-[#F9F9F9]">
-      <div className="h-10 w-10 px-1 py-[3px] my-4 flex justify-center bg-[#F1F1F1] border rounded-md">
-        <ImUser className="h-full text-2xl text-gray-400" />
+    <div
+      id="sidebar"
+      className="hidden h-full xs:flex flex-col items-center bg-[--left-sidebar-bg-color]
+    border-solid border-[--border-gray-5] pb-5 px-1"
+    >
+      <div id="user-profile" className="mb-3 mt-4 relative">
+        <div className="h-8 w-8 bg-[#F1F1F1] rounded-md border flex justify-center items-end">
+          <RiUser3Fill className="h-6 w-6 text-[--icon-color] opacity-40" />
+        </div>
+        <FaCrown className="absolute top-[-4px] right-[-4px] h-3 w-3 text-[#FF8E0A] opacity-70" />
       </div>
-      <ul className="flex flex-col items-center flex-auto w-full">
-        {USER_SELECTED_FEATURES.map((feature) => {
-          const IconComponent = iconsComponents[feature.icon];
+      <div id="user-features" className="flex flex-col flex-auto items-center">
+        {USER_FEATURES.map((feature) => {
+          const IconComponent = feature.icon;
           return (
-            <li
-              className="h-10 w-10 px-1 py-[3px] relative group/item"
+            <div
               key={feature.id}
+              className="p-1 w-10 h-10 flex justify-center items-center"
             >
-              <Link className="flex justify-center" to={"#"}>
-                <IconComponent className="text-2xl text-[#9E9E9E] group-hover/item:text-[#727272]" />
-              </Link>
-            </li>
+              <IconComponent
+                className={`h-[22px] w-[22px] ${
+                  feature.isSelected
+                    ? "text-[--secondary-color]"
+                    : "text-[--icon-color] opacity-50 hover:opacity-70"
+                } ease-in-out duration-150`}
+              />
+            </div>
           );
         })}
-      </ul>
-      <ul className="w-full flex flex-col items-center mb-4">
-        <li className="h-10 w-10 px-1 py-[3px] relative">
-          <button className="flex justify-center w-full h-full">
-            <IoSync className="text-2xl text-[#9E9E9E] hover:text-[#727272]" />
-          </button>
-        </li>
-        <li className="h-10 w-10 px-1 py-[3px] relative">
-          <button className="flex justify-center w-full h-full">
-            <MdNotifications className="text-2xl text-[#9E9E9E] hover:text-[#727272]" />
-          </button>
-        </li>
-        <li className="h-10 w-10 px-1 py-[3px] relative">
-          <button className="flex justify-center w-full h-full">
-            <HiQuestionMarkCircle className="text-2xl text-[#9E9E9E] hover:text-[#727272]" />
-          </button>
-        </li>
-      </ul>
+      </div>
+      <div id="app-features" className="flex-none">
+        {APP_FEATURES.map((feature) => {
+          const IconComponent = feature.icon;
+          return (
+            <div key={feature.id} className="h-[22px] w-[22px] mb-6 last:mb-0">
+              <IconComponent className="h-full w-full text-[--icon-color] opacity-50 hover:opacity-70 ease-in-out duration-150" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
