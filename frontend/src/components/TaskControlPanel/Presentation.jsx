d6ea24479e474a2ed9838e33c5318266dd2f4768
Anish Pawaskar <anishpawaskar@gmail.com>
2024-10-02 22:19:03 +0530

    refactor: styling of TaskControlPanel and add control panel visibility based on screen size

--- frontend/src/components/TaskControlPanel/Presentation.jsx
+++ frontend/src/components/TaskControlPanel/Presentation.jsx
@@ -1,27 +1,71 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { TaskControlList } from "./components/TaskControlList";
-import { selectTask } from "../../store/taskSlice";
+import { selectTask, toggleControlPanel } from "../../store/taskSlice";
 import { TASK_CONTROLS } from "./taskControlPanel.constatns";
 import { TaskControlPanelAccordion } from "./components/TaskControlPanelAccordion";
+import { useEffect } from "react";
+import { useGetScreenSize } from "../../cusstomHooks/useGetScreeeSize";
 
 export const TaskControlPanelPresentation = ({
   isControlPanelVisible,
   overviewList,
 }) => {
-  // TODO: when screen resize is less than 630 and if window resize then hide control panel
+  const dispatch = useDispatch();
+  const [screenSize] = useGetScreenSize();
+
+  useEffect(() => {
+    const handleControlPanelVisibility = () => {
+      const windowWidth = window.innerWidth;
+      if (windowWidth > 630) {
+        dispatch(toggleControlPanel(true));
+      } else {
+        dispatch(toggleControlPanel(false));
+      }
+    };
+    handleControlPanelVisibility();
+    window.addEventListener("resize", handleControlPanelVisibility);
+
+    return () => {
+      window.removeEventListener("resize", handleControlPanelVisibility);
+    };
+  }, []);
+
   return (
-    <div
-      className={`${
-        isControlPanelVisible ? "hidden" : "block"
-      } fixed top-0 left-0 xs:left-[50px] xs:w-[50%] xs:shadow-lg bg-white w-[60%] z-50 h-full  ${
-        isControlPanelVisible ? "xs:hidden" : "xs:block"
-      }  ${
-        isControlPanelVisible ? "sm:block" : "sm:hidden"
-      } sm:static sm:w-[213px] sm:shadow-none border-r`}
-    >
-      <TaskControlList lists={overviewList} />
-      <hr className="my-4" />
-      <TaskControlPanelAccordion title="Lists" />
-    </div>
+    <>
+      <div
+        className={`${
+          isControlPanelVisible ? "block" : "hidden"
+        } fixed top-0 left-0 xs:left-[50px] xs:w-[50%] xs:shadow-lg bg-white w-[305px] z-[100] h-full  ${
+          isControlPanelVisible ? "sm:block" : "sm:hidden"
+        } sm:static sm:w-[213px] sm:shadow-none border-r`}
+      >
+        <TaskControlList lists={overviewList} />
+        <hr className="my-4" />
+        <TaskControlPanelAccordion title="Lists" />
+      </div>
+      {!isControlPanelVisible && (
+        <div
+          classNam={`${
+            isControlPanelVisible ? "hidden" : "block"
+          } top-0 left-0 w-full h-full z-[70] bg-red-400`}
+        ></div>
+      )}
+      {isControlPanelVisible && screenSize.width < 630 && (
+        <div
+          onClick={() => dispatch(toggleControlPanel(!isControlPanelVisible))}
+          className={`absolute top-0 ${
+            screenSize.width < 630 && "left-[50px]"
+          } ${
+            screenSize.width < 500 && "left-0"
+          } z-[70] w-full h-full bg-red-500`}
+        ></div>
+      )}
+    </>
   );
 };
+
+/* 
+  togglePanel width:
+  less than 630: 305px and for transition they have been setting position to absolute and left to -currentWidth
+  grater than 630: 213px same for transition
+*/
