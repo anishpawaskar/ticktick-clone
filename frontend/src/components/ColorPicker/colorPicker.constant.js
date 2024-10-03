8dd9bf10f68983c7fbd5081311f9da51fa15c887
Anish Pawaskar <anishpawaskar@gmail.com>
2024-10-03 23:40:51 +0530

    feat: create color picker component

---
+++ frontend/src/components/ColorPicker/colorPicker.constant.js
@@ -0,0 +1,42 @@
+export const COLORS_DATA = [
+  {
+    id: 1,
+    color: "#fff",
+    name: "Default",
+  },
+  {
+    id: 2,
+    color: "#FF6161",
+    name: "Red",
+  },
+  {
+    id: 3,
+    color: "#FFAC38",
+    name: "Yellow",
+  },
+  {
+    id: 4,
+    color: "#FFD324",
+    name: "Lemon",
+  },
+  {
+    id: 5,
+    color: "#E6EA49",
+    name: "Oliv",
+  },
+  {
+    id: 6,
+    color: "#35D870",
+    name: "Green",
+  },
+  {
+    id: 7,
+    color: "#4CA1FF",
+    name: "Blue",
+  },
+  {
+    id: 8,
+    color: "#6E75F4",
+    name: "Purple",
+  },
+];
