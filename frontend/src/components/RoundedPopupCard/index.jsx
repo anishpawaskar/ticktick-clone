783f5a844c555980593b59e8d6b5a7dfc90874f1
Anish Pawaskar <anishpawaskar@gmail.com>
2024-10-03 22:41:20 +0530

    feat: create popupcard component

---
+++ frontend/src/components/RoundedPopupCard/index.jsx
@@ -0,0 +1,47 @@
+import { RxCross2 } from "react-icons/rx";
+import { useDispatch } from "react-redux";
+import { closeModal } from "../Modal/modalSlice";
+
+export const RoundedPopupCard = ({
+  title,
+  type,
+  handlePopupSave,
+  children,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div
+        className={`absolute top-[20%] left-1/2 -translate-x-1/2 z-[110] bg-white rounded-xl border min-h-[180px] shadow-2xl flex flex-col ${
+          type === "tag" && "w-[90%] xs:w-[380px]"
+        } ${type == "project" && "w-[90%] sm:w-[440px]"}`}
+      >
+        <div className="popup-header flex items-center px-5 min-h-14">
+          <h3 className="flex-[auto] truncate text-base text-[--text-gray] font-bold leading-[1.3]">
+            {title}
+          </h3>
+          <button onClick={() => dispatch(closeModal())}>
+            <RxCross2 className="h-[18px] w-[18px] text-[--icon-color] leading-[16px] opacity-60" />
+          </button>
+        </div>
+        <div className="popup-body px-5 pb-[50px] flex-auto">{children}</div>
+        <div className="popup-footer flex items-center justify-end pt-3 px-5 pb-4">
+          <button
+            onClick={() => dispatch(closeModal())}
+            className="leading-none text-[14px] h-[30px] min-w-[100px] py-1.5 rounded-md border text-[--text-gray-6] bg-transparent hover:bg-[--light-white] active:bg-[#E8E8E8] transition-colors duration-200 ease-in mr-3"
+          >
+            Close
+          </button>
+          <button
+            onClick={() => handlePopupSave()}
+            className="leading-none text-[14px] h-[30px] min-w-[100px] py-1.5 rounded-md border text-[--primary-color] bg-[--secondary-color] disabled:bg-[#B5C7FD] hover:bg-[#6C8EFB] active:bg-[#3E60CD] transition-colors duration-200 ease-in"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+      {/* <div className="absolute top-0 left-0 w-full h-full z-[100]"></div> */}
+    </>
+  );
+};
